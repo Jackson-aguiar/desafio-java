ee4de139cf67d850907ec7a38cc645a641a6c95d
Jackson <jackson.aguiar@sicoob.com.br>
2025-06-14 18:47:37 -0300

    Commit inicial do projeto

---
+++ src/main/java/org/jackson/rest/PautaRest.java
@@ -0,0 +1,15 @@
+package org.jackson.rest;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/pauta")
+@Produces("application/json")
+@Consumes("application/json")
+public class PautaRest {
+
+    public String createPauta(){
+
+    }
+}
